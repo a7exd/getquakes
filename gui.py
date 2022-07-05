c03a817f9bbec61c19020e15d237ebe158af2318
a7exd <a7exdanilov@gmail.com>
2022-07-05 17:32:01 +0300

    add logging, fix methods set and init db_config

--- gui.py
+++ gui.py
@@ -13,6 +13,11 @@ from ui.main_window_ui import Ui_MainWindow
 from ui.db_conn_ui import Ui_Dialog
 import config
 from quakes_from_db import get_data, get_quakes
+import logging.config
+
+
+logging.config.dictConfig(config.LOG_CONFIG)
+log = logging.getLogger('gui_logger')
 
 
 class Window(QMainWindow, Ui_MainWindow):
@@ -27,10 +32,12 @@ class Window(QMainWindow, Ui_MainWindow):
 
     def search_quakes(self) -> None:
         try:
+            log.info(f'Start search for records. '
+                     f'DB connection config: {config.DB}.')
             self.raw_quakes_data = get_data(self._get_query_params())
             self._set_data_into_table()
         except ConnectDatabaseError as exc:
-            print(exc.args[0])  # TODO print into log
+            log.exception(exc)
             self._show_error_dialog(message=f'{exc.args[0]}\n\n'
                                             f'Check connection settings '
                                             f'(File->Settings->Connection) '
@@ -48,14 +55,17 @@ class Window(QMainWindow, Ui_MainWindow):
         file = dialog.getSaveFileName(dir='untitled.txt',
                                       filter=self.file_filter)[0]
         file = Path(file)
+        log.info(f'file to save the quakes: {file}')
         ext = file.suffix
         try:
             quakes = self.get_selected_quakes()
+            log.info(f'{len(quakes)} quakes are ready to save in a storage')
             save_quakes(quakes, storages[ext](file))
+            log.info(f'Writing into the file completed successfully.')
         except (NoSelectedQuakesError, FormatToStrError) as exc:
-            self._show_error_dialog(message=f'Program cannot save the data.\n\n'
-                                            f'{exc.args[0]}')
-                # TODO print into log
+            log.exception(exc)
+            self._show_error_dialog(message=f'Program cannot save the data.\n'
+                                            f'\n{exc.args[0]}')
 
     def _show_error_dialog(self, message):
         title = 'Something went wrong'
@@ -97,13 +107,14 @@ class Window(QMainWindow, Ui_MainWindow):
 
     def _get_selected_quakes_id(self) -> list[str]:
         selected_items_amnt = len(self.tableWidget.selectedItems())
-        if selected_items_amnt == 0 or \
-                selected_items_amnt % self.tableWidget.columnCount() != 0:
+        log.info(f'selected items amount: {selected_items_amnt}')
+        col_count = self.tableWidget.columnCount()
+        if selected_items_amnt == 0 or selected_items_amnt % col_count != 0:
             raise NoSelectedQuakesError('Nothing is selected! At least one row'
                                         ' from the table must be selected!')
+        log.info(f'selected records amount: {selected_items_amnt / col_count}')
         return [self.tableWidget.selectedItems()[i].text()
-                for i in range(selected_items_amnt)
-                if i % self.tableWidget.columnCount() == 0]
+                for i in range(selected_items_amnt) if i % col_count == 0]
 
     def _get_query_params(self) -> tuple[str, ...]:
         from_dt = self.from_dateTime.text()
@@ -112,7 +123,9 @@ class Window(QMainWindow, Ui_MainWindow):
         sta = self.sta_line.text()
         from_mag = f'{self.from_Mag.value()}'
         to_mag = f'{self.to_Mag.value()}'
-        return from_dt, to_dt, comment, sta, from_mag, to_mag
+        params = from_dt, to_dt, comment, sta, from_mag, to_mag
+        log.info(f'query params: {params}')
+        return params
 
     def about(self) -> None:
         QMessageBox.about(
@@ -135,25 +148,30 @@ class ConnectionDialog(QDialog, Ui_Dialog):
         super().__init__(parent)
         self.setupUi(self)
         self._connect_signals_slots()
+        self._init_db_config_gui()
+
+    def _connect_signals_slots(self) -> None:
+        self.buttonBox.accepted.connect(self.set_db_conn_config)
+
+    def _init_db_config_gui(self) -> None:
         self.host_line.setText(config.DB['host'])
         self.port_line.setText(config.DB['port'])
         self.db_name_line.setText(config.DB['database'])
         self.user_line.setText(config.DB['user'])
         self.password_line.setText(config.DB['password'])
 
-    def _connect_signals_slots(self) -> None:
-        self.buttonBox.accepted.connect(self.set_db_conn_config)
-
     def set_db_conn_config(self) -> None:
-        content_lst = Path('config.py').open('r', encoding='utf8').readlines() 
-        db_conf = (('host', self.host_line.text()),
-                   ('port', self.port_line.text()),
-                   ('database', self.db_name_line.text()),
-                   ('user', self.user_line.text()),
-                   ('password', self.password_line.text()))
+        content_lst = Path('config.py').open('r', encoding='utf8').readlines()
+        db_conf = {'host': self.host_line.text(),
+                   'port': self.port_line.text(),
+                   'database': self.db_name_line.text(),
+                   'user': self.user_line.text(),
+                   'password': self.password_line.text()}
 
         with Path('config.py').open('w', encoding='utf8') as config_file:
             config_file.writelines(content_lst[:3])
-            config_file.write(f'DB = {dict(db_conf)}\n')
+            config_file.write(f'DB = {db_conf}\n')
             config_file.writelines(content_lst[4:])
         self.close()
+        config.DB = db_conf
+        log.info(f'db connection config is changed to {db_conf}')
